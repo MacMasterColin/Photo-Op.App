6996bf57526252d6e5769b1c78149ed8d616a7b4
Reagan Davenport <rwdrocks12@gmail.com>
2017-04-21 11:49:15 -0500

    Set up an alert to insert a photo if there is none.

--- PhotoOpApp/DetailViewController.swift
+++ PhotoOpApp/DetailViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var detailDescriptionLabel: UILabel!
+    
+    let imagePicker = UIImagePickerController()
 
 
     func configureView() {
@@ -26,6 +29,12 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        imagePicker.delegate = self
+        if(detailItem?.image == nil)
+        {
+            changePhotoAlert()
+        }
+        imageView.image = detailItem?.image
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,6 +49,35 @@ class DetailViewController: UIViewController {
         }
     }
 
-
+    func changePhotoAlert()
+    {
+        let alert = UIAlertController(title: "Add Photo", message: "", preferredStyle: .alert)
+        let cameraAction = UIAlertAction(title: "Take a Photo Now.", style: .default)
+        {
+            (action) in
+            
+        }
+        let libraryAction = UIAlertAction(title: "Choose from Photo Library.", style: .default)
+        {
+            (action) in
+            self.imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Not Now.", style: .cancel, handler: nil)
+        alert.addAction(cameraAction)
+        alert.addAction(libraryAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imagePicker.dismiss(animated: true)
+        {
+            let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            self.detailItem!.image = selectedImage
+            self.imageView.image = selectedImage
+        }
+    }
+    
 }
 
